b0b20a911a838ab323bdaedcd5940a3c0d34fd59
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-10 22:50:15 +0530

    boatmap

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -8,7 +8,7 @@
 **/
 import { LightningElement, wire } from 'lightning/messageService';
 // import BOATMC from the message channel
-import BOATMC from '@salesforce/memessageChannel/BoatMessageChannel__c';
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
 // Declare the const LONGITUDE_FIELD for the boat's Longitude__s
 
 // Declare the const LATITUDE_FIELD for the boat's Latitude
