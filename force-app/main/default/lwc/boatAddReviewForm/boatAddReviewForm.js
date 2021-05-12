dcbc481494554eee236b2eacee461537309a6fb3
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-12 20:19:54 +0530

    Challenge 11 BoatAddReviewForm

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -1,21 +1,27 @@
 /**
  * @author            : Vrushabh Uprikar
- * @last modified on  : 05-11-2021
+ * @last modified on  : 05-12-2021
  * @last modified by  : Vrushabh Uprikar
  * Modifications Log 
  * Ver   Date         Author             Modification
- * 1.0   05-11-2021   Vrushabh Uprikar   Initial Version
+ * 1.0   05-12-2021   Vrushabh Uprikar   Initial Version
 **/
-import { LightningElement } from 'lwc';
-
 // imports
-// import BOAT_REVIEW_OBJECT from schema - BoatReview__c
-// import NAME_FIELD from schema - BoatReview__c.Name
-// import COMMENT_FIELD from schema - BoatReview__c.Comment__c
-export default class BoatAddReviewForm extends LightningElement {
+import { LightningElement, api } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import BOAT_REVIEW_OBJECT from '@salesforce/schema/BoatReview__c';
+import NAME_FIELD from '@salesforce/schema/BoatReview__c.Name';
+import COMMENT_FIELD from '@salesforce/schema/BoatReview__c.Comment__c';
+// const
+const SUCCESS_TITLE = 'Review Created!';
+const SUCCESS_VARIANT = 'success';
+
+export default class BoatAddReviewForm extends LightningElement
+{
     // Private
-    boatId;
-    rating;
+    @api boatId;
+    rating = 0;
     boatReviewObject = BOAT_REVIEW_OBJECT;
     nameField = NAME_FIELD;
     commentField = COMMENT_FIELD;
@@ -23,28 +29,60 @@ export default class BoatAddReviewForm extends LightningElement {
     labelRating = 'Rating';
 
     // Public Getter and Setter to allow for logic to run on recordId change
-    get recordId() { }
-    set recordId(value) {
+    @api get recordId()
+    {
+        return this.boatId;
+    }
+    set recordId(value)
+    {
         //sets boatId attribute
         //sets boatId assignment
+        this.setAttribute('boatId', value);
+        this.boatId = value;
     }
 
     // Gets user rating input from stars component
-    handleRatingChanged(event) { }
+    handleRatingChanged(event)
+    {
+        this.rating = event.detail.rating;
+    }
 
     // Custom submission handler to properly set Rating
     // This function must prevent the anchor element from navigating to a URL.
     // form to be submitted: lightning-record-edit-form
-    handleSubmit(event) { }
+    handleSubmit(event)
+    {
+        event.preventDefault();
+        const fields = event.detail.fields;
+        fields.Boat__c = this.boatId;
+        fields.Rating__c = this.rating;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    }
 
     // Shows a toast message once form is submitted successfully
     // Dispatches event when a review is created
-    handleSuccess() {
+    handleSuccess()
+    {
         // TODO: dispatch the custom event and show the success message
+        const evt = new ShowToastEvent({
+            title: SUCCESS_TITLE,
+            variant: SUCCESS_VARIANT
+        });
+        this.dispatchEvent(evt);
+        const createreviewEvent = new CustomEvent('createreview');
+        this.dispatchEvent(createreviewEvent);
         this.handleReset();
     }
 
     // Clears form data upon submission
     // TODO: it must reset each lightning-input-field
-    handleReset() { }
+    handleReset()
+    {
+        const inputFields = this.template.querySelectorAll('lightning-input-field');
+        if (inputFields) {
+            inputFields.forEach(field => {
+                field.reset();
+            });
+        }
+    }
 }
