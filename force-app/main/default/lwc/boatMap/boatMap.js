08d380d3e7a91b5de8d121517b9ea32ddcc7e6ce
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-12 15:27:33 +0530

    Challenge 6 boatMap

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -1,18 +1,27 @@
 /**
  * @author            : Vrushabh Uprikar
- * @last modified on  : 05-10-2021
+ * @last modified on  : 05-12-2021
  * @last modified by  : Vrushabh Uprikar
  * Modifications Log 
  * Ver   Date         Author             Modification
  * 1.0   05-10-2021   Vrushabh Uprikar   Initial Version
 **/
-import { LightningElement, wire } from 'lightning/messageService';
+import { LightningElement, wire,api } from 'lwc';
+import { subscribe, unsubscribe, APPLICATION_SCOPE, MessageContext } from 'lightning/messageService';
+import { getRecord } from 'lightning/uiRecordApi';
+
 // import BOATMC from the message channel
 import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+
 // Declare the const LONGITUDE_FIELD for the boat's Longitude__s
+const LONGITUDE_FIELD = 'Boat__c.Geolocation__Longitude__s';
 
 // Declare the const LATITUDE_FIELD for the boat's Latitude
+const LATITUDE_FIELD = 'Boat__c.Geolocation__Latitude__s';
+
 // Declare the const BOAT_FIELDS as a list of [LONGITUDE_FIELD, LATITUDE_FIELD];
+const BOAT_FIELDS = [ LONGITUDE_FIELD,  LATITUDE_FIELD];
+
 export default class BoatMap extends LightningElement
 {
   // private
@@ -21,29 +30,38 @@ export default class BoatMap extends LightningElement
 
   // Getter and Setter to allow for logic to run on recordId change
   // this getter must be public
-  get recordId() {
+  // we will use @api to mark getter public
+  @api
+  get recordId()
+  {
     return this.boatId;
-  }
-  set recordId(value) {
+  }  
+  set recordId(value)
+  {
     this.setAttribute('boatId', value);
     this.boatId = value;
   }
 
-  error = undefined;
-  mapMarkers = [];
+  @api error = undefined;
+  @api mapMarkers = []; 
 
   // Initialize messageContext for Message Service
-
+  @wire(MessageContext)
+  messageContext;
   // Getting record's location to construct map markers using recordId
   // Wire the getRecord method using ('$boatId')
-  wiredRecord({ error, data }) {
+  @wire(getRecord, { recordId: '$boatId', fields: BOAT_FIELDS })
+  wiredRecord({ error, data })
+  {
     // Error handling
-    if (data) {
+    if (data)
+    {
       this.error = undefined;
       const longitude = data.fields.Geolocation__Longitude__s.value;
       const latitude = data.fields.Geolocation__Latitude__s.value;
       this.updateMap(longitude, latitude);
-    } else if (error) {
+    } else if (error)
+    {
       this.error = error;
       this.boatId = undefined;
       this.mapMarkers = [];
@@ -52,24 +70,41 @@ export default class BoatMap extends LightningElement
 
   // Subscribes to the message channel
   subscribeMC() {
+    this.subscription = subscribe(
+      this.messageContext,
+      BOATMC,
+      (message) => {
+        this.boatId = message.recordId;
+      },
+      { scope: APPLICATION_SCOPE }
+    );
     // recordId is populated on Record Pages, and this component
     // should not update when this component is on a record page.
-    if (this.subscription || this.recordId) {
-      return;
-    }
     // Subscribe to the message channel to retrieve the recordId and explicitly assign it to boatId.
   }
 
   // Calls subscribeMC()
-  connectedCallback() {
+  connectedCallback()
+  {
+    // recordId is populated on Record Pages, and this component
+    // should not update when this component is on a record page.
+    if (this.subscription || this.recordId)
+    {
+      return;
+    }
+    // Subscribe to the message channel to retrieve the recordID and assign it to boatId.
     this.subscribeMC();
   }
-
+  
   // Creates the map markers array with the current boat's location for the map.
-  updateMap(Longitude, Latitude) {}
+  updateMap(Longitude, Latitude)
+  {
+    this.mapMarkers = [Longitude, Latitude];
+  }
 
   // Getter method for displaying the map component, or a helper method.
-  get showMap() {
+  get showMap()
+  {
     return this.mapMarkers.length > 0;
   }
 }
