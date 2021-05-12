1284d4adc900df10d4b73035d61a47a960c5432a
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-12 19:42:59 +0530

    Challenge 9 BoatSearch

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,27 +1,45 @@
 /**
  * @author            : Vrushabh Uprikar
- * @last modified on  : 05-11-2021
+ * @last modified on  : 05-12-2021
  * @last modified by  : Vrushabh Uprikar
  * Modifications Log 
  * Ver   Date         Author             Modification
- * 1.0   05-11-2021   Vrushabh Uprikar   Initial Version
+ * 1.0   05-12-2021   Vrushabh Uprikar   Initial Version
 **/
-// imports
-import { LightningElement } from 'lwc';
-
-export default class BoatSearch extends LightningElement
+import { LightningElement, track, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+export default class BoatSearch extends NavigationMixin(LightningElement)
 {
-    isLoading = false;
+    @track isLoading = false;
 
     // Handles loading event
-    handleLoading() { }
+    handleLoading()
+    {
+        this.isLoading = true;
+    }
 
     // Handles done loading event
-    handleDoneLoading() { }
+    handleDoneLoading()
+    {
+        this.isLoading = false;
+    }
 
     // Handles search boat event
     // This custom event comes from the form
-    searchBoats(event) { }
+    searchBoats(event)
+    {
+        const boatTypeId = event.detail.boatTypeId;
+        this.template.querySelector("c-boat-search-results").searchBoats(boatTypeId);
+    }
 
-    createNewBoat() { }
+    createNewBoat()
+    {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new'
+            }
+        });
+    }
 }
