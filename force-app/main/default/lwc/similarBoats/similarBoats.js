49c46a78b2cf3c8d8f9ffea299548dd450f16baf
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-13 17:56:57 +0530

    Challenge 14 SimilarBoats

--- force-app/main/default/lwc/similarBoats/similarBoats.js
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -1,16 +1,21 @@
 /**
  * @author            : Vrushabh Uprikar
- * @last modified on  : 05-11-2021
+ * @last modified on  : 05-13-2021
  * @last modified by  : Vrushabh Uprikar
  * Modifications Log 
  * Ver   Date         Author             Modification
  * 1.0   05-11-2021   Vrushabh Uprikar   Initial Version
 **/
-import { LightningElement } from 'lwc';
-
 // imports
+
+import { api, LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'
+
 // import getSimilarBoats
-export default class SimilarBoats extends LightningElement {
+import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
+
+export default class SimilarBoats extends NavigationMixin(LightningElement)
+{
     // Private
     currentBoat;
     relatedBoats;
@@ -18,27 +23,60 @@ export default class SimilarBoats extends LightningElement {
     error;
 
     // public
-    get recordId() {
+    @api
+    get recordId()
+    {
         // returns the boatId
+        return this.boatId;
     }
-    set recordId(value) {
-        // sets the boatId value
-        // sets the boatId attribute
+    set recordId(value)
+    {
+        //sets boatId attribute
+        this.setAttribute('boatId', value);
+        //sets boatId assignment
+        this.boatId = value;
+        
     }
 
     // public
+    @api
     similarBy;
 
     // Wire custom Apex call, using the import named getSimilarBoats
     // Populates the relatedBoats list
-    similarBoats({ error, data }) { }
-    get getTitle() {
+    @wire(getSimilarBoats, { boatId: '$boatId', similarBy: '$similarBy' })
+    similarBoats({ error, data })
+    {
+        if (data)
+        {
+            this.relatedBoats = data;
+            this.error = undefined;
+        } else if (error)
+        {
+            this.error = error;
+        }
+    }
+
+    get getTitle()
+    {
         return 'Similar boats by ' + this.similarBy;
     }
-    get noBoats() {
+
+    get noBoats()
+    {
         return !(this.relatedBoats && this.relatedBoats.length > 0);
     }
 
     // Navigate to record page
-    openBoatDetailPage(event) { }
+    openBoatDetailPage(event)
+    {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.detail.boatId,
+                objectApiName: BOAT_OBJECT,
+                actionName: 'view'
+            },
+        });
+    }
 }
